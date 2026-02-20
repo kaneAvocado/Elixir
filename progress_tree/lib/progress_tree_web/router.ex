aac38db6906051fe25596e1bc7f911bbade2f1d1
teplov_as <teplov_as@navis.ru>
2026-02-20 20:15:00 +0300

    feat(web): add GenealogyLive.Index catalog page

--- progress_tree/lib/progress_tree_web/router.ex
+++ progress_tree/lib/progress_tree_web/router.ex
@@ -17,21 +17,10 @@ defmodule ProgressTreeWeb.Router do
   scope "/", ProgressTreeWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", GenealogyLive.Index, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", ProgressTreeWeb do
-  #   pipe_through :api
-  # end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:progress_tree, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
